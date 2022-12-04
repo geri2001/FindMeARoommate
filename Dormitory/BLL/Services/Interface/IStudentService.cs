ec3ca3e6601403a0364ecf1d89bc4288149bbe93
Serxhio Gega <serxhio.gega1@gmail.com>
2022-12-04 14:54:44 +0100

    Fix

--- Dormitory/BLL/Services/Interface/IStudentService.cs
+++ Dormitory/BLL/Services/Interface/IStudentService.cs
@@ -5,5 +5,5 @@ namespace DormitoryApi.BLL.Services.Interface;
 public interface IStudentService
 {
     Task<Student> AddAsync(string name, string surname);
-    Task<Student> GetAllAsync();
+    Task<List<Student>> GetAllAsync();
 }
