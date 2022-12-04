a240739aa4aea7aa68d18ffe5d82f0bbca47229c
Alex Sinani <sinanialex383@gmail.com>
2022-12-04 10:02:36 +0100

    Init migrations

---
+++ Dormitory/Migrations/20221204090204_InitDatabase.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Dormitory.Migrations
+{
+    public partial class InitDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Announcements",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    PublishedDate = table.Column<DateTime>(type: "datetime", nullable: false),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Announcements", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Dormitories",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Code = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    MaxCapacity = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Dormitories", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Surname = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Rooms",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Capacity = table.Column<int>(type: "int", nullable: false),
+                    DormitoryID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Rooms", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Rooms_Dormitories",
+                        column: x => x.DormitoryID,
+                        principalTable: "Dormitories",
+                        principalColumn: "ID");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Applications",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    StudentID = table.Column<int>(type: "int", nullable: false),
+                    AnnouncementID = table.Column<int>(type: "int", nullable: false),
+                    ApplicationDate = table.Column<DateTime>(type: "datetime", nullable: false),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Applications", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Applications_Announcements",
+                        column: x => x.AnnouncementID,
+                        principalTable: "Announcements",
+                        principalColumn: "ID");
+                    table.ForeignKey(
+                        name: "FK_Applications_Students",
+                        column: x => x.StudentID,
+                        principalTable: "Students",
+                        principalColumn: "ID");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RoomStudent",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    StudentID = table.Column<int>(type: "int", nullable: false),
+                    RoomID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RoomStudent", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_RoomStudent_Rooms",
+                        column: x => x.RoomID,
+                        principalTable: "Rooms",
+                        principalColumn: "ID");
+                    table.ForeignKey(
+                        name: "FK_RoomStudent_Students",
+                        column: x => x.StudentID,
+                        principalTable: "Students",
+                        principalColumn: "ID");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Applications_AnnouncementID",
+                table: "Applications",
+                column: "AnnouncementID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Applications_StudentID",
+                table: "Applications",
+                column: "StudentID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Rooms_DormitoryID",
+                table: "Rooms",
+                column: "DormitoryID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RoomStudent_RoomID",
+                table: "RoomStudent",
+                column: "RoomID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RoomStudent_StudentID",
+                table: "RoomStudent",
+                column: "StudentID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Applications");
+
+            migrationBuilder.DropTable(
+                name: "RoomStudent");
+
+            migrationBuilder.DropTable(
+                name: "Announcements");
+
+            migrationBuilder.DropTable(
+                name: "Rooms");
+
+            migrationBuilder.DropTable(
+                name: "Students");
+
+            migrationBuilder.DropTable(
+                name: "Dormitories");
+        }
+    }
+}
---
+++ Dormitory/Models/Dormitory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dormitory.Models
+{
+    public partial class Dormitory
+    {
+        public Dormitory()
+        {
+            Rooms = new HashSet<Room>();
+        }
+
+        public int Id { get; set; }
+        public string Code { get; set; } = null!;
+        public string Name { get; set; } = null!;
+        public int MaxCapacity { get; set; }
+
+        public virtual ICollection<Room> Rooms { get; set; }
+    }
+}
